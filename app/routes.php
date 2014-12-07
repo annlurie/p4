81d79605c86b4461706ae5f1f7c9d7ff2e99596d
AnnLurie <lurie@g.harvard.edu>
2014-12-06 19:16:07 -0500

    Initial database stuff - tasks table and some test routes for it

--- app/routes.php
+++ app/routes.php
@@ -11,17 +11,48 @@
 |
 */
 
+/* Real Routes! */
 Route::get('/', function()
 {
 	return View::make('home');
 });
 
+/* DATABASE PRACTICE ROUTES*/
+Route::get('/creating', function() {
+
+    # Instantiate a new Book model class
+    $task = new Task();
+
+    # Set 
+    $task->shortDesc = 'Create the table for tasks_users';
+    $task->longDesc = 'This is one of the things I have to do to finish this project';
+    $task->priority = 3;
+
+    # This is where the Eloquent ORM magic happens
+    $task->save();
+
+    return 'A new book has been added! Check your database to see...';
+
+});
+
+Route::get('/reading', function() {
+
+    $task = Task::where('priority', '=', '2')->first();
+
+    if($task) {
+        return $task->shortDesc;
+    }
+    else {
+        return 'Task not found.';
+    }
+
+});
+/* END DATABASE PRACTICE ROUTES*/
+
 /* Throwaway Routes - delete before submitting. */
 Route::get('/practice', function() {
     echo App::environment();
 });
-
-
 
 /* Routes for testing and debugging. Keep these! */
 
@@ -35,5 +66,19 @@ Route::get('/trigger-error',function() {
 
     # Class Foobar should not exist, so this should create an error
     $foo = new Foobar;
+
+});
+
+/* For testing database presence. */
+Route::get('mysql-test', function() {
+
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
 
 });
